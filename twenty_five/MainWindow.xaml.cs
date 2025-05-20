329347f75d862e81d6c485503a3e13061e995076
Moderator <timurrocev427@gmail.com>
2025-05-20 21:57:13 +0300

    20

--- twenty_five/MainWindow.xaml.cs
+++ twenty_five/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,42 +21,82 @@ namespace twenty_five
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static void nana(int x,int y,GeometryGroup gg,Canvas lalala) 
+        {
+            System.Windows.Shapes.Path pathe = new System.Windows.Shapes.Path();
+            pathe.Data = gg;
+            pathe.Stroke = Brushes.Red;
+            pathe.Fill = Brushes.Black;
+            pathe.RenderTransform = new TranslateTransform(x, y);
+            lalala.Children.Add(pathe);
+        }
         public MainWindow()
         {
             InitializeComponent();
-            
-            poly.Stroke=Brushes.Blue;
-            poly.Fill=Brushes.Red;
-            poly.Points.Add(new Point(20,100));
-            poly.Points.Add(new Point(35, 65));
-            poly.Points.Add(new Point(5, 50));
-            poly.Points.Add(new Point(50, 45));
-            poly.Points.Add(new Point(65, 15));//
-            poly.Points.Add(new Point(80, 45));
-            poly.Points.Add(new Point(125, 50));
-            poly.Points.Add(new Point(95, 65));
-            poly.Points.Add(new Point(110, 100));
-            poly.Points.Add(new Point(65, 80));
-            lalala.Children.Add(poly);
-            Canvas.SetLeft(poly, 10);
-            Canvas.SetTop(poly, 10);
-            Canvas.SetLeft(poly, 50);
-            Canvas.SetTop(poly, 10);
-            Random r=new Random();
-            int t = r.Next(100);
-            for (int i = 0; i < 10; i++) 
-            {
+            class1 class1 = new class1();
+            class1.X = 20;
+            class1.Y = 20;
+            //System.Windows.Shapes.Path pathe = new System.Windows.Shapes.Path();
+            GeometryGroup gg = new GeometryGroup();
+            EllipseGeometry EG = new EllipseGeometry();
+            EG.Center = new Point(30, 30);
+            EG.RadiusX = 20;
+            EG.RadiusY = 20;
+            gg.Children.Add(EG);
+            nana(class1.X, class1.Y, gg, lalala);
+            class1.X = 120;
+            class1.Y = 120;
+            nana(class1.X, class1.Y, gg, lalala);
+            //pathe.Data= gg;
+            //pathe.Stroke=Brushes.Red;
+            //Path star = new Path
+            //{
+            //    Fill = Brushes.Gold,
+            //    Stroke = Brushes.Black,
+            //    StrokeThickness = 2,
+            //    // Форма звезды (M - начало, L - линия, Z - замкнуть)
+            //    Data = Geometry.Parse("M 100,0 L 120,60 L 180,60 L 130,90 L 150,150 L 100,120 L 50,150 L 70,90 L 20,60 L 80,60 Z")
+            //};
 
-                Canvas.SetLeft(poly, t);
-                Canvas.SetTop(poly, t);
-            }
+            //pathe.Fill = Brushes.Black;
+            //lalala.Children.Add(pathe);
+            //Canvas.SetLeft(pathe, 100);
+            //Canvas.SetTop(pathe, 100);
+
+
         }
-
-     Path path= new Path();
-        GeometryGroup Gg= new GeometryGroup(); 
-
-        Polygon poly= new Polygon();  
-       
-    
+      
+        private void ll_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
+    class Pointes
+    {
+        //    private int _x, _y;
+        //    Path _path= new Path();
+        //    //private int X 
+        //    //{
+        //    //    get { return _x; }
+        //    //}
+        //    //public int Y
+        //    //{
+        //    //    get { return _y; }
+        //    //}
+        //    public Pointes(int x, int y,Path path) 
+        //    {
+        //    _x = x;_y = y;
+        //        _path = path;
+        //        Canvas.SetLeft(_path, _x);
+        //        Canvas.SetTop(_path, _y);
+
+        //    }
+
+    }
+}
+class class1
+{
+    private int x ,y;
+    public int X { get { return x; } set { x = value; } }
+    public int Y { get { return y; } set { y = value; } }
 }
